b00d424d57f189468bae924de1041d32db94c603
Vladimir Rudenko <rudenko@farpost.com>
2015-03-21 00:46:54 +1000

    update express

--- app.js
+++ app.js
@@ -4,37 +4,25 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
+var routes = require(__dirname + '/routes/router')(express);
 
-var app = module.exports = express.createServer();
-var io = require('socket.io').listen(app);
+var app = express();
 
 // Configuration
+app.set('env', process.env.MODE || 'development');
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(routes);
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + '/public'));
+
+
+var server = app.listen(app.get('port'), function() {
+    console.log("Express server listening on port " + app.get('port'));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
-
-// Routes
-
-app.get('/', routes.index)
-
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env)
-})
+var io = require('socket.io').listen(server);
 
 //var Game = require('./game/game')
 //var game = new Game(io)
