c8645ec9673d58c15df446695ebe74cb761be8ea
shvetsovdm <sorrows@mail.ru>
2015-04-24 23:21:35 +1000

    ИЗМ тесты, комментарии, индетацию для Field

--- test/field_test.js
+++ test/field_test.js
@@ -2,10 +2,10 @@ var Field   = require('game/field'),
     expect  = require('chai').expect;
 
 describe('Field', function() {
-  describe('type', function() {
-    it('Должен возращать 1, по не понятным причинам', function() {
-      var field = new Field();
-      expect(field.type()).to.equal(1);
+    describe('type', function() {
+        it('Должен возращать 1 как идетификатор поля', function() {
+            var field = new Field();
+            expect(field.type()).to.equal(1);
+        });
     });
-  });
 });
